6fe29414818afa0aaaae82984e02c7b33c470b04
Rosanna Zorzutti <94189034+Ross132020@users.noreply.github.com>
2022-02-08 14:32:56 +0000

    sviluppo funzione get_data

--- esame.py
+++ esame.py
@@ -5,6 +5,7 @@ class CSVTimeSeriesFile():
     def get_data(self):
         #metodo che deve tornare una lista di liste
         #apro il file 
+        #INSERIRE ECCEZIONE APERTURA FILE
         my_file=open(self.name, 'r')
 
         time_series=[]
@@ -13,12 +14,26 @@ class CSVTimeSeriesFile():
         for line in my_file:
             #leggo il file riga per riga
             elements=line.split(',')
-            #divido la riga in due, separando dove c'è la virgola, in modo che torni una lista, ossia la riga, che abbia come primo elemento la data e secondo elemento il numero di passeggeri
+            #divido la riga in due, separando alla virgola, in modo che torni per ogni riga una lista in cui si abbia come primo elemento la data e secondo il numero di passeggeri
 
             #pulisco il carattere newline e eventuali spazi di inizio e fine stringa
             elements[-1]=elements[-1].strip()
 
             if elements[0] != 'date':
             #se non sto processando l'intestazione, associo gli elementi
-                data=elements[0]
+                data=elements[0] 
+                #elements[0]=anno-mese
+
+                #converto la stringa 'passengers' (secondo elemento della lista 'elements') in numero
                 elements[1] = float(elements[1])
+                #associo il nome valore a elements[1]
+                valore=elements[1]
+                
+                #aggiungo gli elementi sottoforma di lista nella lista inizializzata precedentemente
+                time_series.append(elements)
+            
+        for item in time_series:
+            #stampo ogni elemento della serie storica
+            print(item)
+
+        return time_series
